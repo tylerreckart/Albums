9dce33c70d576f84ca72680ed353dc31adec1c93
Tyler Reckart <tyler.reckart@gmail.com>
2023-05-11 08:06:35 -0400

    Initial Commit

---
+++ Albums/AlbumsApp.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumsApp.swift
+//  Albums
+//
+//  Created by Tyler Reckart on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AlbumsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
